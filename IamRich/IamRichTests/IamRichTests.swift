aa7ba00082dc4d0c51c6feb7fa60f272b8eae296
bomsk <112770710+Balzhan31@users.noreply.github.com>
2024-10-01 23:23:36 +0500

    rich

---
+++ IamRich/IamRichTests/IamRichTests.swift
@@ -0,0 +1,17 @@
+//
+//  IamRichTests.swift
+//  IamRichTests
+//
+//  Created by Балжан Бозбашава on 01.10.2024.
+//
+
+import Testing
+@testable import IamRich
+
+struct IamRichTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

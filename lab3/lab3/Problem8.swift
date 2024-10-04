1fe3f523a54f9624743cecec7c109f3789f474ef
bomsk <112770710+Balzhan31@users.noreply.github.com>
2024-10-04 09:59:25 +0500

    Update Problem8.swift

--- lab3/lab3/Problem8.swift
+++ lab3/lab3/Problem8.swift
@@ -3,3 +3,10 @@ import Foundation
 func isPalindrome(_ text: String) -> Bool{
     return text == String(text.reversed())
 }
+if let input = readLine(){
+    if isPalindrome(input){
+        print("yes")
+    }else {
+        print("no")
+    }
+}
